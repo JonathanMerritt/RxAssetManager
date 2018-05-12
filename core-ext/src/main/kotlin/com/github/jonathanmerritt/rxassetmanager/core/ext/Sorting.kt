a660f9b52d1355383fc55bd479099ee4cfa8a5d5
Jonathan Merritt <11r00tt00r11@gmail.com>
2018-05-12 08:12:54 -0400

    Add doc to Sorting.

--- core-ext/src/main/kotlin/com/github/jonathanmerritt/rxassetmanager/core/ext/Sorting.kt
+++ core-ext/src/main/kotlin/com/github/jonathanmerritt/rxassetmanager/core/ext/Sorting.kt
@@ -18,16 +18,47 @@ package com.github.jonathanmerritt.rxassetmanager.core.ext
 
 import com.github.jonathanmerritt.rxassetmanager.core.ext.extensions.depth
 
-
+/**
+ * Comparator to sort thru file path/name strings.
+ *
+ * This class implements a Comparator<String>, and compares file path/name strings based on its' type and values.
+ * @param compares high order compare function.
+ */
 sealed class Sorting(private val compares: (String, String) -> Int = { _, _ -> 0 }) : Comparator<String> {
   override fun compare(s1: String?, s2: String?) = if (s1 == null || s2 == null) 0 else compares(s1, s2)
 }
 
+/**
+ * Will do nothing, denotes no sorting.
+ *
+ * Example:
+ * --------folder/file.txt
+ * --------folder/folder1/file.txt
+ * --------folder/folder1/folder2/file.txt
+ * --------folder/folder1/folder2/file1.txt
+ * --------folder/folder1/file1.txt
+ * --------folder/file1.txt
+ */
 object Normal : Sorting()
+
+/**
+ * Will sort file path/name strings based on their depth in their directories.
+ *
+ * Example:
+ * --------folder/file.txt
+ * --------folder/file1.txt
+ * --------folder/folder1/file.txt
+ * --------folder/folder1/file1.txt
+ * --------folder/folder1/folder2/file.txt
+ * --------folder/folder1/folder2/file1.txt
+ */
 object Depth : Sorting({ s1, s2 ->
   when {
+  // both are in the same directory.
     s1.depth == s2.depth -> 1
+  // s1 is in the directory of s2s' directory.
     s1.depth < s2.depth -> -1
+  // s2 is in the directory of s1s' directory.
     else -> 0
   }
 })
