1f6072d156278b3560506a489481e4398563b332
Jonathan Merritt <11r00tt00r11@gmail.com>
2018-05-13 07:12:43 -0400

    Add doc to interfaces and companions.

--- core-ext/src/main/kotlin/com/github/jonathanmerritt/rxassetmanager/core/ext/IsRxAssetManager.kt
+++ core-ext/src/main/kotlin/com/github/jonathanmerritt/rxassetmanager/core/ext/IsRxAssetManager.kt
@@ -30,63 +30,165 @@ import java.io.File
 import java.io.InputStream
 import com.github.jonathanmerritt.rxassetmanager.core.IsRxAssetManager as isRxAssetManager
 
+/**
+ * Reactive asset manager extended interface.
+ *
+ * This class outlines the extended api for the library.
+ */
 interface IsRxAssetManager : isRxAssetManager {
+  /**
+   * Companion object used for default parameter values.
+   */
   companion object {
+    /**
+     * Default list all = false.
+     */
     const val ALL = false
   }
 
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openString(path: String, mode: Int = MODE): Maybe<String> = Maybe.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openStringPair(path: String, mode: Int = MODE): Maybe<Pair<String, String>> = Maybe.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openBytes(path: String, mode: Int = MODE): Maybe<ByteArray> = Maybe.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openBytesPair(path: String, mode: Int = MODE): Maybe<Pair<String, ByteArray>> = Maybe.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openSave(path: String, mode: Int = MODE, to: String): Maybe<File> = Maybe.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openSavePair(path: String, mode: Int = MODE, to: String): Maybe<Pair<String, File>> = Maybe.empty()
 
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openBitmap(path: String, mode: Int = MODE): Maybe<Bitmap> = Maybe.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openBitmapPair(path: String, mode: Int = MODE): Maybe<Pair<String, Bitmap>> = Maybe.empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listAll(path: String = PATH, sorting: Sorting = Normal): Flowable<String> = empty()
+
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpen(path: String = PATH, mode: Int = MODE, all: Boolean = ALL): Flowable<InputStream> =
       empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenPair(path: String = PATH, mode: Int = MODE, all: Boolean = ALL):
       Flowable<Pair<String, InputStream>> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenString(path: String = PATH, mode: Int = MODE, all: Boolean = ALL): Flowable<String> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenStringPair(path: String = PATH, mode: Int = MODE, all: Boolean = ALL):
       Flowable<Pair<String, String>> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenBytes(path: String = PATH, mode: Int = MODE, all: Boolean = ALL): Flowable<ByteArray> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenBytesPair(path: String = PATH, mode: Int = MODE, all: Boolean = ALL):
       Flowable<Pair<String, ByteArray>> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenSave(path: String = PATH, mode: Int = MODE, to: String, all: Boolean = ALL): Flowable<File> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenSavePair(path: String = PATH, mode: Int = MODE, to: String, all: Boolean = ALL):
       Flowable<Pair<String, File>> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenBitmap(path: String = PATH, mode: Int = MODE, all: Boolean = ALL): Flowable<Bitmap> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenBitmapPair(path: String = PATH, mode: Int = MODE, all: Boolean = ALL):
       Flowable<Pair<String, Bitmap>> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenTypeface(path: String = PATH, all: Boolean = ALL): Flowable<Typeface> = empty()
+
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenTypefacePair(path: String = PATH, all: Boolean = ALL): Flowable<Pair<String, Typeface>> = empty()
+
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenFd(path: String = PATH, all: Boolean = ALL): Flowable<AssetFileDescriptor> = empty()
+
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenFdPair(path: String = PATH, all: Boolean = ALL): Flowable<Pair<String, AssetFileDescriptor>> =
       empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenNonAssetFd(cookie: Int = COOKIE, path: String = PATH,
       all: Boolean = ALL): Flowable<AssetFileDescriptor> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenNonAssetFdPair(cookie: Int = COOKIE, path: String = PATH, all: Boolean = ALL):
       Flowable<Pair<String, AssetFileDescriptor>> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenXmlResourceParser(cookie: Int = COOKIE, path: String = PATH, all: Boolean = ALL):
       Flowable<XmlResourceParser> = empty()
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun listOpenXmlResourceParserPair(cookie: Int = COOKIE, path: String = PATH, all: Boolean = ALL):
       Flowable<Pair<String, XmlResourceParser>> = empty()
 }
--- core/src/main/kotlin/com/github/jonathanmerritt/rxassetmanager/core/IsRxAssetManager.kt
+++ core/src/main/kotlin/com/github/jonathanmerritt/rxassetmanager/core/IsRxAssetManager.kt
@@ -27,25 +27,95 @@ import io.reactivex.Maybe
 import io.reactivex.Maybe.empty
 import java.io.InputStream
 
+/**
+ * Reactive asset manager interface.
+ *
+ * This class outlines the main api for the library.
+ */
 interface IsRxAssetManager {
+  /**
+   * Companion object used for default parameter values.
+   */
   companion object {
+    /**
+     *  Default access mode = [ACCESS_STREAMING].
+     */
     const val MODE = ACCESS_STREAMING
+
+    /**
+     * Default path = "".
+     */
     const val PATH = ""
+
+    /**
+     * Default file cookie = 0.
+     */
     const val COOKIE = 0
   }
 
+  /**
+   * Defaults to an empty flowable.
+   */
   fun getLocales(): Flowable<String> = Flowable.empty()
+
+  /**
+   * Defaults to a complete completable.
+   */
   fun close(): Completable = complete()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun open(path: String, mode: Int = MODE): Maybe<InputStream> = empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openPair(path: String, mode: Int = MODE): Maybe<Pair<String, InputStream>> = empty()
-  infix fun openTypeface(path: String): Maybe<Typeface> = Maybe.empty()
-  infix fun openTypefacePair(path: String): Maybe<Pair<String, Typeface>> = Maybe.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
+  infix fun openTypeface(path: String): Maybe<Typeface> = empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
+  infix fun openTypefacePair(path: String): Maybe<Pair<String, Typeface>> = empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   infix fun openFd(path: String): Maybe<AssetFileDescriptor> = empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   infix fun openFdPair(path: String): Maybe<Pair<String, AssetFileDescriptor>> = empty()
+
+  /**
+   * Defaults to an empty flowable.
+   */
   fun list(path: String = PATH): Flowable<String> = Flowable.empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openNonAssetFd(cookie: Int = COOKIE, path: String): Maybe<AssetFileDescriptor> = empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openNonAssetFdPair(cookie: Int = COOKIE, path: String): Maybe<Pair<String, AssetFileDescriptor>> = empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openXmlResourceParser(cookie: Int = COOKIE, path: String): Maybe<XmlResourceParser> = empty()
+
+  /**
+   * Defaults to an empty maybe.
+   */
   fun openXmlResourceParserPair(cookie: Int = COOKIE, path: String): Maybe<Pair<String, XmlResourceParser>> =
       empty()
 }
